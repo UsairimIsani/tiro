0e03908c3307ae346585359d1beebb470138c682
Usairim Isani <usairim.isani@outlook.com>
2021-01-06 07:15:41 +0500

    trying out a poc.

---
+++ src/bin/poc.rs
@@ -0,0 +1,51 @@
+// use evmap::{new, ReadHandle, WriteHandle};
+// use std::hash;
+// use std::{collections::HashMap, future::Future};
+// use tokio::task::JoinHandle;
+// pub struct TaskGraph<T: Send + hash::Hash + Sync + Eq> {
+//     nodes: HashMap<String, T>,
+//     edges: Vec<Vec<Edge>>,
+// }
+
+// pub struct Edge {
+//     weight: usize,
+//     node: String,
+// }
+// // pub struct Task<T: Future + Send + Sync + 'static> {
+// //     res: tokio::task::JoinHandle<()>,
+// // }
+// type BoxFuture<'a, T> = Box<dyn std::future::Future<Output = T> + Send + 'a>;
+// pub struct Task<'a, T: Send> {
+//     res: JoinHandle<BoxFuture<'a, T>>,
+// }
+// impl<'a, T: Send> Task<'a, T> {
+//     pub fn new<J>(func: J) -> Self
+//     where
+//         J: Future + Send + 'static,
+//         J::Output: Future + Send + 'static,
+//     {
+//         let future: JoinHandle<BoxFuture<<J as Future>>> = tokio::spawn(func);
+//         Task { res: future }
+//     }
+// // }
+// // struct Request<'r>(&'r ());
+
+// // type BoxFuture<'a, T> = std::pin::Pin<Box<dyn std::future::Future<Output = T> + Send + 'a>>;
+
+// // trait FromRequestAsync<'a, 'r>: Sized {
+// //     fn from_request<'fut>(req: &'a Request<'r>) -> BoxFuture<'fut, Option<Self>>
+// //     where
+// //         'a: 'fut;
+// // }
+
+// // impl<'a, 'r, T: FromRequestAsync<'a, 'r>> FromRequestAsync<'a, 'r> for Option<T> {
+// //     fn from_request<'fut>(req: &'a Request<'r>) -> BoxFuture<'fut, Option<Self>>
+// //     where
+// //         'a: 'fut,
+// //     {
+// //         Box::pin(async move { Some(T::from_request(req).await) })
+// //     }
+// // }
+
+#[tokio::main]
+async fn main() {}
