d6b4576cbf8a922f63c314b138221d71506546ce
Usairim Isani <usairim.isani@outlook.com>
2021-01-06 19:24:49 +0500

    Generic Returns for Tasks

--- src/bin/poc.rs
+++ src/bin/poc.rs
@@ -2,11 +2,15 @@ use tiro::prelude::*;
 
 #[tokio::main]
 async fn main() {
-    let task = Task::new(&|| println!("{}", "Yes It Works "));
+    let task = Task::new(|| println!("{}", "Yes It Works "));
+    let task2 = Task::new(move || {
+        let x = rand::random::<u16>();
+        println!("{} {}", "Yes wet Works ", x);
+        x
+    });
     let t1 = tokio::task::spawn(async move { task.execute().await });
-    let task2 = Task::new(&|| println!("{}", "Yes wet Works "));
     let t2 = tokio::task::spawn(async move { task2.execute().await });
-    let task2 = t2.await.unwrap();
-    println!("Hi");
+    let mut task2 = t2.await.unwrap();
+    println!("Hi {}", task2.get_response().unwrap());
     let task = t1.await.unwrap();
 }
--- src/task/task.rs
+++ src/task/task.rs
@@ -1,28 +1,58 @@
 use crate::prelude::Execute;
 use async_trait::async_trait;
-pub struct Task<T>
+pub struct Task<T, R>
 where
-    T: Fn(),
+    T: Fn() -> R,
 {
     func: Box<T>,
+    res: Option<R>,
 }
-unsafe impl<T: Fn()> Send for Task<T> {}
+unsafe impl<T: Fn() -> R, R> Send for Task<T, R> {} // Still Unsure if this is right
 
-impl<T: Fn()> Task<T> {
+impl<T: Fn() -> R, R> Task<T, R> {
     pub fn new(func: T) -> Self {
         let func = Box::new(func);
-        Task { func }
+        Task { func, res: None }
+    }
+    pub fn get_response(&mut self) -> Option<R> {
+        self.res.take()
     }
 }
 
 #[async_trait]
-impl<T: Fn()> Execute for Task<T> {
+impl<T: Fn() -> R, R> Execute for Task<T, R> {
     type Item = Self;
-    async fn execute(self) -> Self::Item {
-        // let random = rand::random::<u8>() as u64;
-        // println!("Random Sleep : {}", random);
-        // std::thread::sleep(std::time::Duration::from_millis(random));
-        (self.func)();
+    async fn execute(mut self) -> Self::Item {
+        self.res = Some((self.func)());
         self
     }
 }
+// use crate::prelude::Execute;
+// use async_trait::async_trait;
+// pub struct Task<T, I, R> // Need to Figure Out PhantomData?
+// where
+//     T: Fn(I) -> R,
+// {
+//     func: Box<T>,
+//     res: Option<R>,
+// }
+// unsafe impl<T: Fn(I) -> R, I, R> Send for Task<T, I, R> {} // Still Unsure if this is right
+
+// impl<T: Fn(I) -> R, I, R> Task<T, I, R> {
+//     pub fn new(func: T) -> Self {
+//         let func = Box::new(func);
+//         Task { func, res: None }
+//     }
+//     pub fn get_response(&mut self) -> Option<R> {
+//         self.res.take()
+//     }
+// }
+
+// #[async_trait]
+// impl<T: Fn(I) -> R, I, R> Execute for Task<T, I, R> {
+//     type Item = Self;
+//     async fn execute(mut self, param: I) -> Self::Item {
+//         self.res = Some((self.func)(param));
+//         self
+//     }
+// }
